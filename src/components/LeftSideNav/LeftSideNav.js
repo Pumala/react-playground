a9270552910360181e2d566695fe3a81a6e3b80d
Lyn <carolyn_lam1@homedepot.com>
2019-03-30 12:09:42 -0400

    add side nav and home comp

---
+++ src/components/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,15 @@
+import React, { useSet } from 'react';
+
+const LeftSideNav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    hello
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default LeftSideNav;
